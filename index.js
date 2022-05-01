7741f224009f9eba1a4d58b26e9b7cbac6241b28
ZoranUTF8 <janjiczoran23@gmail.com>
2022-05-01 20:25:38 +0200

    Set up cherrio scraper

---
+++ index.js
@@ -0,0 +1,44 @@
+const PORT = 8000;
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const app = express();
+// array to hold news articles
+const articles = [];
+
+// * API ROUTES
+
+app.get("/", (req, res) => {
+  res.json("ola");
+});
+
+app.get("/news", async (req, res) => {
+  console.log("====================================");
+  console.log("GET NEWS");
+  console.log("====================================");
+  try {
+    const result = await axios.get(
+      "https://sustainabilitynews.eu/category/news/"
+    );
+    const html = result.data;
+
+    // pass HTML data to cherrio which will allow us to pick out what elements we want
+    const $ = cheerio.load(html);
+
+    $(".entry-title", html).each((index, element) => {
+      const title = $(this).text();
+      const link = $(this).children("a").attr("href");
+      console.log($(element).text());
+    });
+
+    // console.log($(".entry-title ").text());
+
+    // console.log($(".css-ye6x8s").children().first().html());
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server on : ${PORT}`);
+});
--- readme.rd
+++ readme.rd
@@ -4,3 +4,9 @@ dependencies:
 -Cheerio parses markup and provides an API for traversing/manipulating the resulting data structure
 
 
+*express
+-Express is a minimal and flexible Node.js web application framework that provides a robust set of
+
+*axios
+-Axios is a promise-based HTTP Client for node.js and the browser. 
+
