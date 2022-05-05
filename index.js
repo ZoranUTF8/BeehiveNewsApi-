5d84074b89ed43b6b4e2297684efcc56948e2d1c
ZoranUTF8 <janjiczoran23@gmail.com>
2022-05-05 16:05:10 +0200

    Finished all 4 news sources

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ const sources = [
   {
     name: "utopiaEnergie",
     address: "https://utopia.de/energie/",
-    source: "utopia",
+    source: "utopia-energie",
   },
   {
     name: "utopiaWissenTechnik",
     address: "https://utopia.de/wissen-technik/",
-    source: "utopia",
+    source: "utopia-technik",
   },
 ];
 
@@ -41,38 +41,38 @@ sources.forEach(async function (source) {
 
   switch (source.name) {
     case "sustainabilitynews":
-      // $(".td-image-wrap ", sourceHtml).each((_, element) => {
-      //   const imgLink = $(element).children("img").attr("data-img-url");
-      //   const title = $(element).children("img").attr("title");
-      //   const newsLink = $(element).attr("href");
-      //   const newsSource = source.source;
-      //   const area = "international";
+      $(".td-image-wrap ", sourceHtml).each((_, element) => {
+        const imgLink = $(element).children("img").attr("data-img-url");
+        const title = $(element).children("img").attr("title");
+        const newsLink = $(element).attr("href");
+        const newsSource = source.source;
+        const area = "international";
 
-      //   articles.push({ title, imgLink, newsLink, newsSource, area });
-      // });
+        articles.push({ title, imgLink, newsLink, newsSource, area });
+      });
 
       break;
 
     case "oekonews":
-      // $(".uebersicht", sourceHtml).each((_, element) => {
-      //   const imgLink =
-      //     sources[1].base +
-      //       $(element).children("a").children("img").attr("src") ||
-      //     $(element).children("h2").children("a").attr("href");
+      $(".uebersicht", sourceHtml).each((_, element) => {
+        const imgLink =
+          sources[1].base +
+            $(element).children("a").children("img").attr("src") ||
+          $(element).children("h2").children("a").attr("href");
 
-      //   const newsLink = $(element).children("a").attr("href").slice(2);
-      //   const title = $(element).children("h2").text();
-      //   const newsSource = source.source;
-      //   const area = "europe";
+        const newsLink = $(element).children("a").attr("href").slice(2);
+        const title = $(element).children("h2").text();
+        const newsSource = source.source;
+        const area = "europe";
 
-      //   articles.push({
-      //     title,
-      //     imgLink,
-      //     newsLink,
-      //     newsSource,
-      //     area,
-      //   });
-      // });
+        articles.push({
+          title,
+          imgLink,
+          newsLink,
+          newsSource,
+          area,
+        });
+      });
       break;
 
     case "utopiaEnergie":
@@ -108,6 +108,36 @@ sources.forEach(async function (source) {
       break;
 
     case "utopiaWissenTechnik":
+      $(".standard-post", sourceHtml).each((_, element) => {
+        const imgLink =
+          $(element)
+            .children()
+            .first()
+            .children()
+            .children()
+            .attr("data-lazy-src") ||
+          $(element).children().first().children().children().attr("src");
+
+        const newsLink = $(element)
+          .children()
+          .last()
+          .children()
+          .children("a")
+          .attr("href");
+
+        const title = $(element).children().last().children("h3").text();
+        const newsSource = source.source;
+        const area = "europe";
+
+        articles.push({
+          title,
+          imgLink,
+          newsLink,
+          newsSource,
+          area,
+        });
+      });
+
       break;
     default:
       console.err("No such option (sources.foreach())");
