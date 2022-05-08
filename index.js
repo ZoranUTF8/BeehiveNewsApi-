8b0f311d8145a33b45e4616206edaf1acd56ce12
Zoran <66476222+ZoranUTF8@users.noreply.github.com>
2022-05-08 19:29:44 +0200

    Get news date, news short description

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-"use strict";
-
 const PORT = process.env.PORT || 8000;
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
 const app = express();
-// array that hold news articles
+//* array that hold news articles
 const articles = [];
-// array that hold news sources
+//* array that hold news sources
 const sources = [
   {
     name: "Sustainabilitynews",
     address: "https://sustainabilitynews.eu/category/news/",
     source: "sustainabilitynews",
+    area: "international",
   },
   {
     name: "Oekonews",
@@ -32,6 +31,27 @@ const sources = [
   },
 ];
 
+//* Returns the substring with our defaultNewsDescriptionLength as specified.
+//* If you want the length of the news longer then just increase the defaultNewsDescriptionLength to your wishes
+function trimNewsDescription(newsParam) {
+  const defaultNewsDescriptionLength = 150;
+  let newsParamOut = "";
+
+  newsParam.length > defaultNewsDescriptionLength
+    ? (newsParamOut =
+        newsParam.substring(0, defaultNewsDescriptionLength - 3) + "...")
+    : (newsParamOut = newsParam);
+
+  return newsParamOut;
+}
+function fixDateSpaces(stringParam) {
+  return stringParam.replace(/\s/g, "");
+}
+function removeExtraLines(stringParam) {
+  return stringParam.replace(/[\n\r]/g, "");
+}
+
+//* Loop trough each of the news sources array and scrape the data that we want
 sources.forEach(async function (source) {
   // fetch source page html
   const result = await axios.get(source.address);
@@ -43,102 +63,143 @@ sources.forEach(async function (source) {
   // Retrive the elements data as needed
   switch (source.name) {
     case "Sustainabilitynews":
-      $(".td-image-wrap ", sourceHtml).each((_, element) => {
-        const imgLink = $(element).children("img").attr("data-img-url");
-        const title = $(element).children("img").attr("title");
-        const newsLink = $(element).attr("href");
-        const newsSource = source.source;
-        const area = "international";
+    
+      $(".td_module_10", sourceHtml).each((_, element) => {
+        const imgLink = $(element)
+          .children()
+          .first()
+          .children()
+          .children("img")
+          .attr("data-img-url");
 
-        articles.push({ title, imgLink, newsLink, newsSource, area });
+        const title = $(element)
+          .children()
+          .first()
+          .children()
+          .children("img")
+          .attr("title");
+
+        let newsDate = $(element)
+          .children()
+          .last()
+          .children(".td-module-meta-info")
+          .text();
+        const newsDateOut = fixDateSpaces(newsDate);
+
+        let newsDescriptionIn = $(element)
+          .children()
+          .last()
+          .children(".td-excerpt")
+          .text();
+
+        const newsDescriptionOut = trimNewsDescription(
+          removeExtraLines(newsDescriptionIn)
+        );
+
+        const newsLink = $(element).children().first().children().attr("href");
+
+        const newsSource = source?.source;
+
+        const area = source?.area;
+
+        articles.push({
+          title,
+          imgLink,
+          newsLink,
+          newsDateOut,
+          newsDescriptionOut,
+          newsSource,
+          area,
+        });
+
       });
 
       break;
 
     case "Oekonews":
-      $(".uebersicht", sourceHtml).each((_, element) => {
-        const imgLink =
-          sources[1].base +
-            $(element).children("a").children("img").attr("src") ||
-          $(element).children("h2").children("a").attr("href");
+      // $(".uebersicht", sourceHtml).each((_, element) => {
+      //   const imgLink =
+      //     sources[1].base +
+      //       $(element).children("a").children("img").attr("src") ||
+      //     $(element).children("h2").children("a").attr("href");
 
-        const newsLink = $(element).children("a").attr("href").slice(2);
-        const title = $(element).children("h2").text();
-        const newsSource = source.source;
-        const area = "europe";
+      //   const newsLink = $(element).children("a").attr("href").slice(2);
+      //   const title = $(element).children("h2").text();
+      //   const newsSource = source.source;
+      //   const area = "europe";
 
-        articles.push({
-          title,
-          imgLink,
-          newsLink,
-          newsSource,
-          area,
-        });
-      });
+      //   articles.push({
+      //     title,
+      //     imgLink,
+      //     newsLink,
+      //     newsSource,
+      //     area,
+      //   });
+      // });
       break;
 
     case "UtopiaEnergie":
-      $(".standard-post", sourceHtml).each((_, element) => {
-        const imgLink =
-          $(element)
-            .children()
-            .first()
-            .children()
-            .children()
-            .attr("data-lazy-src") ||
-          $(element).children().first().children().children().attr("src");
+      // $(".standard-post", sourceHtml).each((_, element) => {
+      //   const imgLink =
+      //     $(element)
+      //       .children()
+      //       .first()
+      //       .children()
+      //       .children()
+      //       .attr("data-lazy-src") ||
+      //     $(element).children().first().children().children().attr("src");
 
-        const newsLink = $(element)
-          .children()
-          .last()
-          .children()
-          .children("a")
-          .attr("href");
+      //   const newsLink = $(element)
+      //     .children()
+      //     .last()
+      //     .children()
+      //     .children("a")
+      //     .attr("href");
 
-        const title = $(element).children().last().children("h3").text();
-        const newsSource = source.source;
-        const area = "europe";
+      //   const title = $(element).children().last().children("h3").text();
+      //   const newsSource = source.source;
+      //   const area = "europe";
 
-        articles.push({
-          title,
-          imgLink,
-          newsLink,
-          newsSource,
-          area,
-        });
-      });
+      //   articles.push({
+      //     title,
+      //     imgLink,
+      //     newsLink,
+      //     newsSource,
+      //     area,
+      //   });
+      // });
       break;
 
     case "UtopiaWissenTechnik":
-      $(".standard-post", sourceHtml).each((_, element) => {
-        const imgLink =
-          $(element)
-            .children()
-            .first()
-            .children()
-            .children()
-            .attr("data-lazy-src") ||
-          $(element).children().first().children().children().attr("src");
+      // $(".standard-post", sourceHtml).each((_, element) => {
+      //   const imgLink =
+      //     $(element)
+      //       .children()
+      //       .first()
+      //       .children()
+      //       .children()
+      //       .attr("data-lazy-src") ||
+      //     $(element).children().first().children().children().attr("src");
 
-        const newsLink = $(element)
-          .children()
-          .last()
-          .children()
-          .children("a")
-          .attr("href");
+      //   const newsLink = $(element)
+      //     .children()
+      //     .last()
+      //     .children()
+      //     .children("a")
+      //     .attr("href");
 
-        const title = $(element).children().last().children("h3").text();
-        const newsSource = source.source;
-        const area = "europe";
+      //   const title = $(element).children().last().children("h3").text();
+      //   const newsSource = source.source;
+      //   const area = "europe";
 
-        articles.push({
-          title,
-          imgLink,
-          newsLink,
-          newsSource,
-          area,
-        });
-      });
+      //   articles.push({
+      //     title,
+      //     imgLink,
+      //     newsLink,
+      //     newsSource,
+      //     area,
+      //   });
+      // });
 
       break;
     default:
@@ -156,15 +217,16 @@ app.get("/news", (req, res) => {
   res.json(articles);
 });
 
-app.get("/news/:sourceName", (req, res) => {
-  const { sourceName } = req.params;
+// Todo
+// app.get("/news/:sourceName", (req, res) => {
+//   const { sourceName } = req.params;
 
-  const source = sources.filter((source) => source.name === sourceName);
+//   const source = sources.filter((source) => source.name === sourceName);
 
-  //  GET SPECIFIC NEWS IF NEEDED
-});
+//    GET SPECIFIC NEWS IF NEEDED
+// });
 
 //
 app.listen(PORT, () => {
-  console.log(`Server on : ${PORT}`);
+  console.log(`Server on  PORT: ${PORT}`);
 });
